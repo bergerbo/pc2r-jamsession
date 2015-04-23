b80e84d2f2f77995f9e470be852a3fb3720bd7b8
Boris Berger <berger.boris@orange.fr>
2015-04-23 17:56:28 +0200

    Now using proper character escaping for args

--- JamSession-Client/src/pc2r/upmc/jamsession/network/MessageBuilder.java
+++ JamSession-Client/src/pc2r/upmc/jamsession/network/MessageBuilder.java
@@ -1,69 +1,59 @@
 package pc2r.upmc.jamsession.network;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MessageBuilder {
 	private final static char SEP = '/';
-	
-	
-	public static String build(Message msg){
+
+	public static String build(Message msg) {
 		StringBuilder sb = new StringBuilder();
-		
+
 		sb.append(msg.getCmd());
-		
+
 		ArrayList<String> args = msg.getArgs();
-		
-		if(!args.isEmpty()){
+
+		if (!args.isEmpty()) {
 			sb.append(SEP);
-			for(String arg : args ){
-				sb.append(arg);
+			for (String arg : args) {
+				sb.append(escape(arg));
 				sb.append(SEP);
 			}
 		}
 		sb.append("\n");
 		return sb.toString();
 	}
-	
-	
-	public  static Message parse(String str) throws UnkownCommandException{
-		int i;
-		char c;
+
+	int i;
+
+	public static Message parse(String s) throws UnkownCommandException {
 		String cmd;
 		ArrayList<String> args;
-		StringBuilder sb = new StringBuilder();
+		String str = unescape(s);
 		
-		//Parse Command
-		for(i = 0; i < str.length(); i++){
-			c = str.charAt(i);
-			if(c == SEP){
-				i++;
-				break;
-			}
-			sb.append(c);
-		}
-	
-		cmd = sb.toString();
+		String[] parts = str.split("/");
+
+		cmd = parts[0];
 		if (!Command.knownCommand(cmd))
 			throw new UnkownCommandException("Unknown command : " + cmd);
-		
-		
-		//Parse args
-		sb = new StringBuilder();
-		args = new ArrayList<>();
-		while(i < str.length()){
-			c = str.charAt(i);
-			
-			if(c == SEP){
-				args.add(sb.toString());
-				sb = new StringBuilder();
-				i++;
-				continue;
-			}
-				
-			sb.append(c);
-			i++;
-		}
-		
+
+		args = new ArrayList<String>(Arrays.asList(
+				Arrays.copyOfRange(parts, 1, parts.length)));;
+
 		return new Message(cmd, args);
+	}
+
+	private static String escape(String s) {
+		String escaped;
+		escaped = s.replace("\\", "\\\\");
+		escaped = escaped.replace("/", "\\/");
+		return escaped;
+	}
+
+	private static String unescape(String s) {
+		String unescaped;
+		unescaped = s.replace("\\/", "/");
+		unescaped = unescaped.replace("\\\\", "\\");
+		return unescaped;
 	}
 }
